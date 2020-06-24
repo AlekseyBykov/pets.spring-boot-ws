85a4098cc43f3984dfb1ac00411a345119f739fb
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-06-24 14:55:41 +0300

    Refactoring.

--- soap-ws/src/main/java/alekseybykov/portfolio/springboot/ws/callback/ServiceSideUTPCallback.java
+++ soap-ws/src/main/java/alekseybykov/portfolio/springboot/ws/callback/ServiceSideUTPCallback.java
@@ -1,4 +1,4 @@
-package alekseybykov.portfolio.springboot.ws.config;
+package alekseybykov.portfolio.springboot.ws.callback;
 
 import org.apache.wss4j.common.ext.WSPasswordCallback;
 
@@ -9,13 +9,13 @@ import java.util.Map;
 
 /**
  * @author Aleksey Bykov
- * @since 23.06.2020
+ * @since 24.06.2020
  */
-public class UsernameTokenPasswordCallback implements CallbackHandler {
+public class ServiceSideUTPCallback implements CallbackHandler {
 
 	private Map<String, String> credentials = new HashMap<>();
 
-	public UsernameTokenPasswordCallback() {
+	public ServiceSideUTPCallback() {
 		credentials.put("some username", "some password");
 	}
 
